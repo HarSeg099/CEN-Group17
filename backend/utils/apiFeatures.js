5df72c180fbd90553cba20d4ae780ab78c2e17f6
Estephany <70328029+fany19@users.noreply.github.com>
2021-04-09 10:21:52 -0400

    Update apiFeatures.js
    allowing search by tittle

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -6,7 +6,7 @@ class APIFeatures {
 
     search(){
         const keyword = this.queryStr.keyword ? {
-            author: {
+            title: {
                 $regex: this.queryStr.keyword,
                 $options: 'i'
             }
